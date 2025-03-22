4f89fffda2c6f9bd8ed37d88baaba589c7d344d2
Ben sinvani <Bensinvani7@gmail.com>
2025-03-22 21:33:25 +0200

    project completed

---
+++ backend/src/controllers/stat.controller.js
@@ -0,0 +1,48 @@
+import { Album } from "../models/album.model.js";
+import { Song } from "../models/song.model.js";
+import { User } from "../models/user.model.js";
+
+export const getStats = async (req, res, next) => {
+  try {
+    // count number of songs
+    // const totalSongs = await Song.countDocuments();
+    // count number of users
+    // const totalUsers = await User.countDocuments();
+    // count number of albums
+    // const totalAlbums = await Album.countDocuments();
+
+    // optimized code to fetch all at once
+    const [totalSongs, totalAlbums, totalUsers, uniqueArtists] =
+      await Promise.all([
+        Song.countDocuments(),
+        Album.countDocuments(),
+        User.countDocuments(),
+
+        Song.aggregate([
+          {
+            $unionWith: {
+              coll: "albums",
+              pipeline: [],
+            },
+          },
+          {
+            $group: {
+              _id: "$artist",
+            },
+          },
+          {
+            $count: "count",
+          },
+        ]),
+      ]);
+
+    res.status(200).json({
+      totalAlbums,
+      totalSongs,
+      totalUsers,
+      totalArtists: uniqueArtists[0]?.count || 0,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
